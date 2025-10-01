8af43f4f0aef2e9cc1f8be6c50d1dd9a6df5936c
Fabio Cesar Reis de Carvalho <53885395+fareis2000@users.noreply.github.com>
2025-10-01 11:59:31 -0300

    Add files via upload

---
+++ Modularização/Pacotes/__init__.py
@@ -0,0 +1,11 @@
+from Pacotes.packages import soma_do_modulo, variavel, nova_variavel
+
+
+
+
+# print(
+#     'Voce importou o pacote Pacotes', __name__
+# )
+
+# def dobra(x):
+#     return x * 2
--- Modularização/aula98_m.py
+++ Modularização/aula98_m.py
@@ -1,4 +1,18 @@
-print(123)
+#from sys import path
 
-variavel = 'Luiz'
+# #import Pacotes.packages
+# #from Pacotes import packages
+#from Pacotes.packages import *
 
+# # from aula99_package.modulo import soma_do_modulo
+
+# print(*path, sep='\n')
+#print(soma_do_modulo(1, 2))
+#print(Pacotes.packages.soma_do_modulo(1, 2))
+#print(packages.soma_do_modulo(1, 2))
+#print(variavel)
+#print(nova_variavel)
+
+import Pacotes
+
+print(Pacotes.soma_do_modulo(1, 2))
